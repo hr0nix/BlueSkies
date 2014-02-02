f0d0a64d212cc94548c71bf1f18d9e9542eb2cd7
Ilia Pozhilov <ilyapoz@gmail.com>
2014-02-02 15:53:07 +0400

    Dialog sequence showing boilerplate.

--- scripts/tutor.js
+++ scripts/tutor.js
@@ -1,16 +1,61 @@
 function startTutor() {
-    $("#dialog-rightclick").dialog({
+    var lastShownDialog;
+    var skipTutor = false;
+
+    function closeDialog() {
+        $(this).dialog("close");
+    }
+
+    var commonOptions = {
         resizable: false,
         draggable: false,
         minHeight: 0,
-        modal: true,
+        modal: false,
         width: "auto",
         show: "fade",
         hide: "fade",
-        position: {
-            of: "#map-canvas-container",
-            my: "top center",
-            at: "center top+100"
+        buttons: [ {
+            text: "Skip tutor",
+            click: function() {
+                skipTutor = true;
+                $(this).dialog("close");
+            }
+        }, {
+            text: "Next",
+            click: closeDialog
         }
+        ]
+    };
+    var specificOptions = {
+        "language": {
+            position: {
+                of: "#right-panel"
+            }
+        },
+        "rightclick": {
+            position: {
+                of: "#map-canvas-container",
+                my: "top center",
+                at: "center top+100"
+            }
+        },
+        "reachset": {
+            position: {
+                of: "#display-ui-element-buttons",
+                my: "right center",
+                at: "left center"
+            }
+        }
+    };
+    
+    var allDialogs = $("#dialogs > div");
+
+    lastShownDialog = allDialogs.first();
+
+    allDialogs.each(function(){
+        //var next = $(this).next();
+        var options = specificOptions[$(this).attr("id").replace("dialog-","")];
+        //$.extend(options, commonOptions);
+        $(this).dialog(options, commonOptions);
     });
 }
